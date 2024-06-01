d35ffa736d22952a63e9baf33b8631ed94cd9da9
y-matsuno525 <y.matsuno525@gmail.com>
2024-06-01 12:21:48 +0900

    データベースを追加した

--- race_prediction/race/models.py
+++ race_prediction/race/models.py
@@ -1,10 +1,10 @@
 from typing import Any
 from django.db import models
-from django.contrib.auth.models import (BaseUserManager,
-                                        AbstractBaseUser,
-                                        PermissionsMixin)
+from django.contrib.auth.models import (BaseUserManager,AbstractBaseUser,PermissionsMixin)
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.models import User
+from django.conf import settings
+
 
 
 #後でマイグレーションするのを忘れずに！
@@ -22,5 +22,9 @@ class Horse(models.Model):
     name=models.CharField(max_length=100)
     vote_count=models.IntegerField(default=0)
 
-
+#ユーザーの投票履歴
+class Race_history(models.Model):
+    race=models.ForeignKey(Race,on_delete=models.CASCADE)#投票したレース
+    user=models.OneToOneField(settings.AUTH_USER_MODEL)#投票したユーザー
+    voted_horse=models.ForeignKey(Horse,on_delete=models.CASCADE)#投票先の馬名
 
